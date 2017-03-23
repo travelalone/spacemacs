ca047349da42249db0c7ca5bf356ca964564b520
haomiao <zhanghm1994@icloud.com>
2017-03-23 13:45:24 +0800

    add_funcs.el

---
+++ haomiao/funcs.el
@@ -0,0 +1,29 @@
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (org-display-inline-images)
+  (setq filename
+        (concat
+         (make-temp-name
+          (concat (file-name-nondirectory (buffer-file-name))
+                  "_imgs/"
+                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (unless (file-exists-p (file-name-directory filename))
+    (make-directory (file-name-directory filename)))
+					; take screenshot
+  (if (eq system-type 'darwin)
+      (progn
+	(call-process-shell-command "screencapture" nil nil nil nil " -s " (concat
+									    "\"" filename "\"" ))
+	(call-process-shell-command "convert" nil nil nil nil (concat "\"" filename "\" -resize  \"50%\"" ) (concat "\"" filename "\"" ))
+	))
+  (if (eq system-type 'gnu/linux)
+      (call-process "import" nil nil nil filename))
+					; insert into file if correctly taken
+  (if (file-exists-p filename)
+      (insert (concat "[[file:" filename "]]")))
+  (org-display-inline-images)
+  )
+
+(global-set-key (kbd "C-c s c") 'my-org-screenshot)
